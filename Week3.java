3a9bb47f0e96c4add0a5c93b22b0eef8e131d88a
Ronaldy-sys <54905550+Ronaldy-sys@users.noreply.github.com>
2019-09-11 12:34:00 -0400

    Add files via upload

---
+++ Week3.java
@@ -0,0 +1,22 @@
+
+public class Week3 {
+	public static void main(String [] args) {
+//		System.out.println(78+98); // 176
+//		System.out.println('a' + 'b'); //196
+//		System.out.println('a' - 'b'); // -1
+//		System.out.println('0' + '0'); // 96
+//		System.out.println('a' + 1); //98
+//		System.out.println('a'); //a
+		
+		int num = 0;
+		double dollars = 1.50;
+		String name1 = "Ronaldy";
+		String name2 = "Julio";
+		
+		System.out.printf("I have %d girlfriends, cause I'm not loved %n", num);
+		System.out.printf("Justan has $%.2f in her pocket, to buy a Sprite", dollars);
+		
+		System.out.printf("%n%s is %d year older than %s", name1, 1, name2);
+		
+	}
+}
